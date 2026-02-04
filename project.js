9baa3bbd0cfbd93d9070e27f6a7ef8fc612ddf24
PYticPH <PYticPH@gmail.com>
2026-02-04 09:29:26 -0500

    add: project data (project list)

---
+++ project.js
@@ -0,0 +1,49 @@
+//Project list
+
+const imgPlaceholder = "images/placeholder.avif"
+const githubUrl = "https://github.com/PYticPH/"
+
+export const project = [
+  {
+    name: "Profile card",
+    image: imgPlaceholder,
+    githubRepo: `${githubUrl}profile-card`,
+    tech: "React"
+  },
+  {
+    name: "Simple Basketball Scoreboard",
+    image: imgPlaceholder,
+    githubRepo: `${githubUrl}simple-basketball-scoreboard`,
+    tech: "Html, CSS, JavaScript"
+  },
+  {
+    name: "Unit converter",
+    image: imgPlaceholder,
+    githubRepo: `${githubUrl}unit-converter`,
+    tech: "Html, CSS, JavaScript"
+  },
+  {
+    name: "Stonewall",
+    image: imgPlaceholder,
+    githubRepo: `${githubUrl}stonewall`,
+    tech: "Html, CSS, JavaScript"
+  },
+  {
+    name: "Instagram Clone",
+    image: imgPlaceholder,
+    githubRepo: `${githubUrl}Instagram-clone`,
+    tech: "Html, CSS, JavaScript"
+  },
+  {
+    name: "Restaurant App",
+    image: imgPlaceholder,
+    githubRepo: `${githubUrl}restaurant-app`,
+    tech: "Html, CSS, JavaScript"
+  },
+  {
+    name: "learning journal",
+    image: imgPlaceholder,
+    githubRepo: `${githubUrl}learning-journal`,
+    tech: "Html, CSS, JavaScript"
+  }
+]

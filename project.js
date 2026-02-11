ec2c5fdd2150b24c1178f6ff7e18046fce794ddf
PYticPH <pyticph@gmail.com>
2026-02-11 13:14:15 +0100

    fix: Fix minor typo

--- project.js
+++ project.js
@@ -1,49 +1,49 @@
 //Project list
 
-const imgPlaceholder = "images/plceholder.avif"
-const githubUrl = "https://github.com/PYticPH/"
+const imgPlaceholder = "images/plceholder.avif";
+const githubUrl = "https://github.com/PYticPH/";
 
 export const project = [
   {
     name: "Profile card",
     image: imgPlaceholder,
     githubRepo: `${githubUrl}profile-card`,
-    tech: "React"
+    tech: "React",
   },
   {
-    name: "Simple Basketball Scoreboard",
+    name: "Simple basketball scoreboard",
     image: imgPlaceholder,
     githubRepo: `${githubUrl}simple-basketball-scoreboard`,
-    tech: "Html, CSS, JavaScript"
+    tech: "Html, CSS, JavaScript",
   },
   {
     name: "Unit converter",
     image: imgPlaceholder,
     githubRepo: `${githubUrl}unit-converter`,
-    tech: "Html, CSS, JavaScript"
+    tech: "Html, CSS, JavaScript",
   },
   {
     name: "Stonewall",
     image: imgPlaceholder,
     githubRepo: `${githubUrl}stonewall`,
-    tech: "Html, CSS, JavaScript"
+    tech: "Html, CSS, JavaScript",
   },
   {
-    name: "Instagram Clone",
+    name: "Instagram clone",
     image: imgPlaceholder,
     githubRepo: `${githubUrl}Instagram-clone`,
-    tech: "Html, CSS, JavaScript"
+    tech: "Html, CSS, JavaScript",
   },
   {
     name: "Restaurant App",
     image: imgPlaceholder,
     githubRepo: `${githubUrl}restaurant-app`,
-    tech: "Html, CSS, JavaScript"
+    tech: "Html, CSS, JavaScript",
   },
   {
-    name: "learning journal",
+    name: "Learning journal",
     image: imgPlaceholder,
     githubRepo: `${githubUrl}learning-journal`,
-    tech: "Html, CSS, JavaScript"
-  }
-]
+    tech: "Html, CSS, JavaScript",
+  },
+];
